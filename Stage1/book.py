a9a8801edf0e7d414c2b0f3bef3277bbd9ed59c5
IrisesD <2824326242@qq.com>
2022-11-08 21:21:28 +0800

    Upload Stage1

--- Stage1/book.py
+++ Stage1/book.py
@@ -5,26 +5,76 @@ import json
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 book_attrs = []
+
+proxyHost = "43.248.99.36"
+proxyPort = 56129
+proxyMeta = "http://%(host)s:%(port)s" % {
+
+    "host": proxyHost,
+    "port": proxyPort,
+}
+proxies = {
+    "http": proxyMeta,
+    "https": proxyMeta
+}
+ipaddr = ""
+with open("ip.txt") as f:
+    ipaddr = f.readlines()
+j = -1
 with open("Book_id.txt") as f:
     str = f.readlines()
+    k = 0
     for i in tqdm(range(len(str))):
+        k += 1
         id = str[i][:-1]
         headers = {"User-Agent": "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)"}
         url = "https://book.douban.com/subject/" + id
-        r = requests.get(url, headers=headers, verify=False)
+        if k % 30 == 0:
+            j += 1
+            proxyHost = ipaddr[j][:-7]
+            proxyPort = int(ipaddr[j][-6:-1])
+            proxyMeta = "http://%(host)s:%(port)s" % {
+                
+                "host": proxyHost,
+                "port": proxyPort,
+            }
+            proxies = {
+                "http": proxyMeta,
+                "https": proxyMeta
+            }
+        r = requests.get(url, headers=headers, proxies=proxies)
         content = r.content.decode("utf-8")
         soup = BeautifulSoup(content,'html.parser')
-        intro = soup.find('span',attrs={'class':'all hidden'})[-2]
-        author_intro = soup.find('span',attrs={'class':'all hidden'})[-1]
+        if len(soup.findAll('div',attrs={'class':'intro'})) >= 2:
+            intro = soup.findAll('div',attrs={'class':'intro'})[-2]
+            author_intro = soup.findAll('div',attrs={'class':'intro'})[-1]    
+        else:
+            intro = soup.find('div',attrs={'class':'intro'})
+            author_intro = None
         book_name = soup.find('meta',attrs={'property':'og:title'})
         info = soup.find('div',attrs={'id':'info'})
         rating = soup.find('div',attrs={'class':"rating_self clearfix"})
         book_attrs.append({})
-        book_attrs[-1]['name'] = book_name.attrs['content']
+        if book_name != None:
+            book_attrs[-1]['name'] = book_name.attrs['content']
+        else:
+            book_attrs[-1]['name'] = "Not Found"
         book_attrs[-1]['id'] = id
-        book_attrs[-1]['info'] = info.text
-        book_attrs[-1]['intro'] = intro.text
-        book_attrs[-1]['author intro'] = author_intro.text
-        book_attrs[-1]['rating'] = rating.text
+        if info != None:
+            book_attrs[-1]['info'] = info.text
+        else:
+            book_attrs[-1]['info'] = "None"
+        if intro != None:
+            book_attrs[-1]['intro'] = intro.text
+        else:
+            book_attrs[-1]['intro'] = "None"
+        if author_intro != None:
+            book_attrs[-1]['author intro'] = author_intro.text
+        else:
+            book_attrs[-1]['author intro'] = "None"
+        if rating != None:
+            book_attrs[-1]['rating'] = rating.text
+        else:
+            book_attrs[-1]['rating'] = "由于某些原因没有评分"
 with open("book.json", "w") as fp:
     json.dump(book_attrs,fp,ensure_ascii=False,indent=4)
--- Stage1/movie.py
+++ Stage1/movie.py
@@ -6,13 +6,44 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 movie_attrs = []
+
+proxyHost = "43.248.79.156"
+proxyPort = 62906
+proxyMeta = "http://%(host)s:%(port)s" % {
+
+    "host": proxyHost,
+    "port": proxyPort,
+}
+proxies = {
+    "http": proxyMeta,
+    "https": proxyMeta
+}
+ipaddr = ""
+with open("ip.txt") as f:
+    ipaddr = f.readlines()
+j = -1
 with open("Movie_id.txt") as f:
     str = f.readlines()
-    for i in tqdm(range(10)):
+    k = 0
+    for i in tqdm(range(len(str))):
+        k += 1
         id = str[i][:-1]
         headers = {"User-Agent": "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)"}
         url = "https://movie.douban.com/subject/" + id
-        r = requests.get(url,headers=headers)
+        if k % 30 == 0:
+            j += 1
+            proxyHost = ipaddr[j][:-7]
+            proxyPort = int(ipaddr[j][-6:-1])
+            proxyMeta = "http://%(host)s:%(port)s" % {
+
+                "host": proxyHost,
+                "port": proxyPort,
+            }
+            proxies = {
+                "http": proxyMeta,
+                "https": proxyMeta
+            }
+        r = requests.get(url,headers=headers,proxies=proxies)
         content = r.content.decode("utf-8")
         soup = BeautifulSoup(content,'html.parser')
         intro = soup.find('div',attrs={'class':'intro'})
@@ -24,13 +55,28 @@ with open("Movie_id.txt") as f:
         info = soup.find('div',attrs={'id':'info'})
         rating = soup.find('div',attrs={'class':"rating_self clearfix"})
         movie_attrs.append({})
-        movie_attrs[-1]['name'] = movie_name.attrs['content']
+        if movie_name != None:
+            movie_attrs[-1]['name'] = movie_name.attrs['content']
+        else:
+            movie_attrs[-1]['name'] = "未找到"
         movie_attrs[-1]['id'] = id
-        movie_attrs[-1]['info'] = info.text
+        if info != None:
+            movie_attrs[-1]['info'] = info.text
+        else:
+            movie_attrs[-1]['info'] = "None"
         movie_attrs[-1]['staff'] = []
-        for i in range(len(staff_name)):
-            movie_attrs[-1]['staff'].append(staff_name[i].text + ":" + staff_role[i].text)
-        movie_attrs[-1]['intro'] = intro.text
-        movie_attrs[-1]['rating'] = rating.text
+        if staff_name != None and staff_role != None:
+            for i in range(len(staff_name)):
+                movie_attrs[-1]['staff'].append(staff_name[i].text + ":" + staff_role[i].text)
+        else:
+            movie_attrs[-1]['staff'].append('None')
+        if intro != None:
+            movie_attrs[-1]['intro'] = intro.text
+        else:
+            movie_attrs[-1]['intro'] = "None"
+        if rating != None:
+            movie_attrs[-1]['rating'] = rating.text
+        else:
+            movie_attrs[-1]['rating'] = "由于某些原因没有评分"
 with open("movie.json", "w") as fp:
     json.dump(movie_attrs,fp,ensure_ascii=False,indent=4)
